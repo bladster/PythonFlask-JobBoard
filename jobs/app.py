022c37c345f38db8d7255102da54b82be6117319
jan <murdochjj@gmail.com>
2019-06-14 17:24:36 +0100

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -7,12 +7,13 @@ app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
-    if connection=="None":
+    if connection==None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.rowfactory = sqlite3.Row
+    return connection
 
-def execute_sql(sql, values=(), commit=False, single=False:
-    connection=open_connection()
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit==True:
         results = connection.commit()
